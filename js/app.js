15846d23f5ebffa99b1a97a8aef0a39a60dfefd1
Ivan Shen <ivanshen@inside-173-157.wireless.utoronto.ca>
2016-01-23 00:29:02 -0500

    Init

---
+++ js/app.js
@@ -0,0 +1,80 @@
+var app = angular.module("myGame", []);
+app.controller ('TableController', function ($scope){
+     $scope.testArr = [{
+        'first': [{
+            'value': generate(),
+            'rolle': generate()
+        },
+        {
+            'value': generate(),
+            'rolle': generate()
+        }, 
+        {
+            'value': generate(),
+            'rolle': generate()
+        },
+        {
+            'value': generate(),
+            'rolle': generate()
+        },
+        {
+            'value': generate(),
+            'rolle': generate()
+        },
+        {
+            'value': generate(),
+            'rolle': generate()
+        }]
+    }, {
+        'second': [{
+            'value': generate(),
+            'rolle': generate()
+        }, {
+            'value': generate(),
+            'rolle': generate()
+        }, {
+            'value': generate(),
+            'rolle': generate()
+        },
+                  {
+            'value': generate(),
+            'rolle': generate()
+        },
+        {
+            'value': generate(),
+            'rolle': generate()
+        },
+        {
+            'value': generate(),
+            'rolle': generate()
+        }]
+    }, {
+        'third': [{
+            'value': generate(),
+            'rolle': generate()
+        }, {
+            'value': generate(),
+            'rolle': generate()
+        }, {
+            'value': generate(),
+            'rolle': generate()
+        },
+                  {
+            'value': generate(),
+            'rolle': generate()
+        },
+        {
+            'value': generate(),
+            'rolle': generate()
+        },
+        {
+            'value': generate(),
+            'rolle': generate()
+        }]
+    }];
+
+});
+
+var generate = function() {
+    return Math.floor(Math.random() * (10 - (-3)) + (-3));
+}
