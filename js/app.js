24175345941f96be2825c650541f2ea8076aa7a0
Ivan Shen <ivanshen@inside-173-157.wireless.utoronto.ca>
2016-01-23 17:42:55 -0500

    final product

--- js/app.js
+++ js/app.js
@@ -1,204 +1,92 @@
 var app = angular.module("myGame", []);
-app.controller ('TableController', function ($scope){
-
+app.controller ('TableController', function ($scope){ 
     
-     $scope.testArr = [{
-        'first': [{
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        }, 
-        {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        }]
-    }, {
-        'second': [{
-            'value': generate(),
-            'rolle': generate()
-        }, {
-            'value': generate(),
-            'rolle': generate()
-        }, {
-            'value': generate(),
-            'rolle': generate()
-        },
-                  {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        }]
-    }, {
-        'third': [{
-            'value': generate(),
-            'rolle': generate()
-        }, {
-            'value': generate(),
-            'rolle': generate()
-        }, {
-            'value': generate(),
-            'rolle': generate()
-        },
-                  {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        }]
-    }];
-<<<<<<< HEAD
 
-    $scope.firstValue = 0;
+    $scope.firstValue = null;
     $scope.secondClick = false;
-    $scope.clickElement = function(t1, outerindex, innerindex){
+    $scope.x1 = 0;
+    $scope.y1 = 0;
+    $scope.points = 0;
+    $scope.timeleft = true;
+    var startTimer = function(duration, display) {
+        var timer = duration, minutes, seconds;
+        var myTimer = setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+            if (timer-- <= 0) {
+                clearInterval(myTimer);
+                $scope.timeleft = false;
+            }
+    }, 1000);
+}
+$scope.start = function () {
+    $scope.testArr = [
+         [generate(), generate(),generate(),generate(),generate(),generate()],
+         [generate(), generate(),generate(),generate(),generate(),generate()],
+         [generate(), generate(),generate(),generate(),generate(),generate()],
+         [generate(), generate(),generate(),generate(),generate(),generate()],
+         [generate(), generate(),generate(),generate(),generate(),generate()],
+         [generate(), generate(),generate(),generate(),generate(),generate()]
+     ];
+    $scope.newNumber = function(){
+        $scope.testArr = [
+         [generate(), generate(),generate(),generate(),generate(),generate()],
+         [generate(), generate(),generate(),generate(),generate(),generate()],
+         [generate(), generate(),generate(),generate(),generate(),generate()],
+         [generate(), generate(),generate(),generate(),generate(),generate()],
+         [generate(), generate(),generate(),generate(),generate(),generate()],
+         [generate(), generate(),generate(),generate(),generate(),generate()]
+     ];
+    };
+    var oneMinute = 60,
+    display = document.querySelector('#time');
+    startTimer(oneMinute, display);
+    /*$scope.$apply(function() {
+        $("td").click(function(){
+            console.log(this)
+            $(this).toggleClass("focus");
+        });
+    });*/
+}
         
-        if($scope.secondClick){
-            $scope.firstValue += t1;
-            $scope.secondClick = false;
-            console.log($scope.firstValue);
+    $scope.clickElement = function(t1, $event, x, y){
+        if ($scope.timeleft && $scope.firstValue == null){
+            $scope.firstValue = t1;
+            $scope.x1 = x;
+            $scope.y1 = y;
+            t1.isSelected = true;
+        }
+        else if($scope.timeleft && $scope.firstValue != null){
+                $scope.secondValue = t1;
+                $scope.firstValue.isSelected = false;
+                $scope.secondValue.isSelected =  false;
+                if($scope.firstValue.value + $scope.secondValue.value == 7){
+                    $scope.testArr[$scope.x1][$scope.y1] = generate();
+                    $scope.testArr[x][y] = generate();
+                    $scope.points++;
+                    $scope.firstValue = null;
+                    t1.isSelected = true;
+                }
+                $scope.firstValue = null;
             
         }else{
-            $scope.firstValue = t1;
-            $scope.secondClick = true;
-            console.log($scope.firstValue);
-            console.log("index: " + outerindex + innerindex);
-        }
-        
-
+            console.log("Something went wrong....")
+        };
     }
-
-
-=======
-    $scope.newNumber = function(){
-        $scope.testArr = [{
-        'first': [{
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        }, 
-        {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        }]
-    }, {
-        'second': [{
-            'value': generate(),
-            'rolle': generate()
-        }, {
-            'value': generate(),
-            'rolle': generate()
-        }, {
-            'value': generate(),
-            'rolle': generate()
-        },
-                  {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        }]
-    }, {
-        'third': [{
-            'value': generate(),
-            'rolle': generate()
-        }, {
-            'value': generate(),
-            'rolle': generate()
-        }, {
-            'value': generate(),
-            'rolle': generate()
-        },
-                  {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        },
-        {
-            'value': generate(),
-            'rolle': generate()
-        }]
-    }];
-    };
->>>>>>> master
+    $scope.getClass = function(isSelected){
+        if (isSelected){
+            return "selected";
+        }
+        return "notSelected";
+    }
 });
 
 var generate = function() {
-    return Math.floor(Math.random() * (10 - (-3)) + (-3));
+    return {value: Math.floor(Math.random() * (10 - (-3)) + (-3)), isSelected: false}
 }
-<<<<<<< HEAD
-=======
-
-var startTimer = function(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-window.onload = function () {
-    var oneMinute = 60,
-        display = document.querySelector('#time');
-    startTimer(oneMinute, display);
-};
->>>>>>> master
+var get
