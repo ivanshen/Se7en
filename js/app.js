46f722e50b66e49616dc3a2dda096cf18b069eb5
Ivan Shen <ivanshen@inside-173-157.wireless.utoronto.ca>
2016-01-23 02:25:09 -0500

    added timer

--- js/app.js
+++ js/app.js
@@ -151,3 +151,25 @@ app.controller ('TableController', function ($scope){
 var generate = function() {
     return Math.floor(Math.random() * (10 - (-3)) + (-3));
 }
+
+var startTimer = function(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+window.onload = function () {
+    var oneMinute = 60,
+        display = document.querySelector('#time');
+    startTimer(oneMinute, display);
+};
